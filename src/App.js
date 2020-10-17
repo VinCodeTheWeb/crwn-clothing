f87dfbde5a9f374334761904efb95b8866e6ed24
Vincenzo Pellegrini <vincenzo@wearefram.com>
2020-10-17 09:49:20 +0200

    updating our app with shop page, colletion-preview component - collection-item component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
 const App = () => (
   <Router>
     <>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </>
   </Router>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 
 import Directory from '../../components/directory/directory.component';
 
