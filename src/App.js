7291342f4660078c1e1d60d380cf3fbdfa00ca22
Vincenzo Pellegrini <vincenzo@wearefram.com>
2020-10-20 08:53:00 +0200

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+
+import Header from './components/header/header.component';
+
 const App = () => (
   <Router>
     <>
+      <Header /> 
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
