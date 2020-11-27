a9717f1ac4ad125ff1f1d5021a810bed4bdb3adc
Vincenzo Pellegrini <vincenzo@wearefram.com>
2020-11-27 09:29:22 +0100

    Updated shope data with new data and udpated cartReducer with utils function

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import CustomButton from '../cutsom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, imageUrl, price }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-        }}
-    />
-  
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-  </div>
-);
+const CollectionItem = ({ id, name, imageUrl, price }) => {
+  const dispatch = useDispatch();
 
+  const addToCart = () => dispatch(addItem({ id, name, imageUrl, price }));
+
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+          }}
+      />
+    
+        <div className='collection-footer'>
+          <span className='name'>{name}</span>
+          <span className='price'>{price}</span>
+      </div>
+      <CustomButton inverted onClick={addToCart}> Add to cart </CustomButton>
+    </div>
+  );
+}
 export default CollectionItem;
