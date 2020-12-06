487a0849cf66f86a0778a5e0219e727ccb89ba62
Vincenzo Pellegrini <vincenzo@wearefram.com>
2020-12-06 07:44:31 +0100

    removing redux logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = createStore(
   persistedReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  process.env.NODE_ENV === 'development' &&
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export const persistor = persistStore(store);
